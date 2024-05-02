252d812c773ea41e7cf26e7b653c17da531403a3
Michalis <michalisjk@gmail.com>
2024-05-02 21:55:25 +0100

    Ball and bounces added

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(width=800, height=600)
-screen.title("Computertonic Tennis")
+screen.title("Computertronic Tennis")
 screen.bgcolor("black")
 screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -18,15 +21,17 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_on = True
 while game_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
+    # Detect Collision with Wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
 
-
-
-
-
-
-
+    # Detect collision with right paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 
 screen.exitonclick()
